7963cf5273eab814bdc09dafc13d1ca2e468dcd6
Varga Zalán <vargazalan58@gmail.com>
2025-01-24 13:35:50 +0100

    Tesseract OCR added

--- app/src/main/java/com/example/sumit/ui/DemoScreen.kt
+++ app/src/main/java/com/example/sumit/ui/DemoScreen.kt
@@ -1,5 +1,6 @@
 package com.example.sumit.ui
 
+import android.graphics.BitmapFactory
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,11 +12,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sumit.R
+import java.io.File
 
 @Preview(showBackground = true)
 @Composable
@@ -24,6 +27,17 @@ fun DemoScreen(
     demoViewModel: DemoViewModel = viewModel()
 ) {
     val demoUiState by demoViewModel.uiState.collectAsState()
+    val context = LocalContext.current
+    val dataDir = File(context.filesDir, "tessdata")
+    dataDir.mkdirs()
+    val copiedFile = File(dataDir, "eng.traineddata")
+    if (!copiedFile.exists()) {
+        context.assets.open("eng.traineddata").use { input ->
+            copiedFile.outputStream().use { output ->
+                input.copyTo(output, 1024)
+            }
+        }
+    }
 
     Column(
         modifier = modifier.fillMaxSize(),
@@ -35,9 +49,12 @@ fun DemoScreen(
             fontSize = 26.sp
         )
 
-        Image(painter = painterResource(R.drawable.test), contentDescription = null)
+        Image(painter = painterResource(R.drawable.a35), contentDescription = null)
 
-        Button(onClick = demoViewModel::recognizeImage) {
+        Button(onClick = { demoViewModel.recognizeImage(
+            context.filesDir.absolutePath,
+            BitmapFactory.decodeResource(context.resources, R.drawable.a35)
+        ) }) {
             Text(text = "Start")
         }
 
--- app/src/main/java/com/example/sumit/ui/DemoViewModel.kt
+++ app/src/main/java/com/example/sumit/ui/DemoViewModel.kt
@@ -1,15 +1,29 @@
 package com.example.sumit.ui
 
+import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
+import com.googlecode.tesseract.android.TessBaseAPI
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class DemoViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(DemoUiState())
     val uiState: StateFlow<DemoUiState> = _uiState.asStateFlow()
 
-    fun recognizeImage() {
+    fun recognizeImage(dataPath: String, image: Bitmap) {
+        val tess = TessBaseAPI()
+        if (!tess.init(dataPath, "eng")) {
+            tess.recycle()
+            return
+        }
+        tess.setImage(image)
+        val text = tess.utF8Text
+        tess.recycle()
 
+        _uiState.update { currentState ->
+            currentState.copy(result = text)
+        }
     }
 }
