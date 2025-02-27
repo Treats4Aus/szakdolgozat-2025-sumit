5a4a9ca0f76ef48bbdb291f4c36f4328397bda69
Varga Zalán <vargazalan58@gmail.com>
2025-02-27 21:19:51 +0100

    Added TopAppBar

--- app/src/main/java/com/example/sumit/ui/SumItApp.kt
+++ app/src/main/java/com/example/sumit/ui/SumItApp.kt
@@ -1,15 +1,24 @@
 package com.example.sumit.ui
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.sumit.ui.home.HomeScreen
 import com.example.sumit.ui.scan.ScanScreen
@@ -23,7 +32,17 @@ enum class SumItScreen {
 fun SumItApp(
     navController: NavHostController = rememberNavController()
 ) {
-    Scaffold { innerPadding ->
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = SumItScreen.valueOf(
+        backStackEntry?.destination?.route ?: SumItScreen.Home.name
+    )
+
+    Scaffold(topBar = {
+        SumItAppBar(
+            currentScreen = currentScreen,
+            canNavigateBack = navController.previousBackStackEntry != null,
+            navigateUp = { navController.navigateUp() })
+    }) { innerPadding ->
         NavHost(
             navController = navController,
             startDestination = SumItScreen.Home.name,
@@ -39,6 +58,32 @@ fun SumItApp(
             }
         }
     }
+}
+
+@Composable
+fun SumItAppBar(
+    currentScreen: SumItScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(currentScreen.name) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "back"
+                    )
+                }
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
